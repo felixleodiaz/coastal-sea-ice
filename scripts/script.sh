5430451f5a4c3c84f7168794be1d3d0918cfca55
Felix Diaz <felixleodiaz@gmail.com>
2025-06-19 19:40:36 -0400

    create script.sh

---
+++ scripts/script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=get_nsidc              # job name
+#SBATCH --partition=hpc                   # partition or queue name
+#SBATCH --nodes=1                         # num of nodes
+#SBATCH --ntasks-per-node=1               # num of tasks per node
+#SBATCH --cpus-per-task=5                 # num of CPU cores per task
+#SBATCH --mem-per-cpu=2G                  # memory per CPU requested
+#SBATCH --time=1:00:00                    # max runtime
+#SBATCH --output=job_output_%j.out        # job output
+#SBATCH --error=job_output_%j.err         # job error output
+#SBATCH --mail-type=END, FAIL             # send email at job completion
+#SBATCH --mail-user=fld1@williams.edu     # email address for notifications
+
+# load python interpreter
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate coastal-ice
+
+# run scripts
+python time-average-variations.py
