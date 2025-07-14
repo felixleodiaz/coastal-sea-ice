fixed error in time average script

scripts/time-average-variations.py
@@ -1,8 +1,4 @@
 # general libraries
-import earthaccess
-import xarray as xr
-import dask
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style('dark')
@@ -12,10 +8,10 @@ from CNSIDC import ice_get, ice_combine
 
 # fetch data 1990-2020 for NSIDC 0051 and 0079
 
-ds51 = ice_get('NSIDC-0051', '1990-01-01', '2020-12-31')
+ds51 = ice_get('NSIDC-0051', '1990-01-01', '1990-01-15')
 ds51 = ice_combine(ds51)
 
-ds79 = ice_get('NSIDC-0079', '1990-01-01', '2020-12-31')
+ds79 = ice_get('NSIDC-0079', '1990-01-01', '1990-01-15')
 ds79 = ice_combine(ds79)
 
 # average entire dataset
@@ -27,20 +23,18 @@ ds79_avg = ds79.icecon.mean(dim='time')
 
 ds_avg_dif = ds51_avg - ds79_avg
 
-# Plot 1
+# Plot 1: NSIDC 0051
 fig, ax = plt.subplots(figsize=(8, 6))
 ds51_avg.plot(ax=ax, cmap='PuBu', cbar_kwargs={'label': 'Sea Ice Concentration (%)'})
 ax.set_title("Average Sea Ice Concentration NSIDC 0051: 1990–2020")
 fig.savefig("../figures/NSIDC0051_TOTAL_AVG.png", dpi=300, bbox_inches='tight')
-plt.show(fig)
 plt.close(fig)
 
-# Plot 2
+# Plot 2: NSIDC 0079
 fig, ax = plt.subplots(figsize=(8, 6))
 ds79_avg.plot(ax=ax, cmap='PuBu', cbar_kwargs={'label': 'Sea Ice Concentration (%)'})
 ax.set_title("Average Sea Ice Concentration NSIDC 0079: 1990–2020")
 fig.savefig("../figures/NSIDC0079_TOTAL_AVG.png", dpi=300, bbox_inches='tight')
-plt.show(fig)
 plt.close(fig)
 
 # Plot 3: Difference
@@ -48,5 +42,4 @@ fig, ax = plt.subplots(figsize=(8, 6))
 ds_avg_dif.plot(ax=ax, cmap='bwr', cbar_kwargs={'label': 'Sea Ice Concentration (%)'})
 ax.set_title("Difference in Tot Avg Sea Ice: NSIDC 0079 and NSIDC 0051: 1990–2020")
 fig.savefig("../figures/TOTAL_AVG_DIF.png", dpi=300, bbox_inches='tight')
-plt.show(fig)
 plt.close(fig)